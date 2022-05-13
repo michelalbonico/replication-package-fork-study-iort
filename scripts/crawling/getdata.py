bc194c3a98bed28f3f9bef89558a99921e81c48d
Juliano Soares <70970603+JulianoSoarez@users.noreply.github>
2022-05-13 09:21:46 -0300

    Script de buscaconcluído, falta gerar um csv com as informações.

--- scripts/crawling/getdata.py
+++ scripts/crawling/getdata.py
@@ -1,3 +1,4 @@
+from ast import Index
 import sqlite3
 from click import echo
 from selenium import webdriver
@@ -25,22 +26,23 @@ for line in f:
 repositoryArray = []
 next_exists = False
 urls = []
-fork_url = []
+#fork_url = []
+url_fork = []
 
 
 # , 'Iot', 'Internet of things', 'Internet das coisas'
 
 
 class Repository:
-    def __init__(self,forks, url_fork, last_update_source):
+    def __init__(self,forks, url_fork, last_update_source, last_update_fork, commits_behind):
         
 
         #self.source_project_url = source_project_url
         self.forks = forks
         self.url_fork = url_fork
         self.last_update_source = last_update_source
-        #self.last_update_fork = last_update_fork
-        #self.commits_behind = commits_behind
+        self.last_update_fork = last_update_fork
+        self.commits_behind = commits_behind
 
 def insert_data(q):
     q = q.replace(" ", "_")
@@ -52,6 +54,8 @@ def insert_data(q):
         forks int,
         url_fork varchar(240),
         last_update_source varchar(240),
+        last_update_fork varchar(240),
+        commits_behind varchar(240)
     );
     ''')
 
@@ -60,9 +64,9 @@ def insert_data(q):
     for repo in repositoryArray:
         print(repo.link)
         cursor.execute('''
-            INSERT INTO repository ( forks, url_fork, last_update_source)
-             VALUES (?,?,?)
-            ''', (repo.forks, repo.url_fork, repo.last_update_source))
+            INSERT INTO repository ( forks, url_fork, last_update_source, last_update_fork, commits_behind)
+             VALUES (?,?,?,?,?)
+            ''', (repo.forks, repo.url_fork, repo.last_update_source, repo.last_update_fork, repo.commits_behind))
 
         conn.commit()
 
@@ -73,7 +77,7 @@ def insert_data(q):
 def get_data(urls):
     for url in urls:
         chrome.get(url)
-        time.sleep(5)
+        time.sleep(3)
        
         print(url)
         print('-----------')
@@ -108,23 +112,53 @@ def get_data(urls):
             fork_element.click()
 
         
-        time.sleep(5)
+        time.sleep(3)
 
-        
-        fork_item = chrome.find_element_by_class_name("Layout-main")
-        fork_list = fork_item.find_elements_by_class_name("network")
-        for fork in fork_list:
-            class_repo = fork.find_elements_by_class_name("repo")[1]
-            url_fork = class_repo.find_element_by_css_selector('a').get_attribute('href')
-        
-            fork_url.append(url_fork)
-            print(url_fork)
-            print('-----------')
-        
-        repositoryArray.append(
-            Repository(last_update_source, forks, url_fork))
+        class_repo = chrome.find_elements_by_class_name("repo")
+        for repo in class_repo:
+            tag_a = repo.find_elements_by_css_selector('a')[2]
+            link_fork = tag_a.get_attribute('href')
+            url_fork.append(link_fork)
 
-        time.sleep(5)
+    del url_fork[0]     
+    print(url_fork)
+    print('-----------')
+
+    for page_fork in url_fork:
+        print('fork')
+        chrome.get(page_fork)
+        print("\n--- Searching for " + page_fork + " ---\n")
+        time.sleep(3)
+        try:
+            last_update_fork = chrome.find_elements_by_css_selector('a[href*="commit"] > relative-time')[0].text
+        except NoSuchElementException:
+            last_update_fork = 'None' 
+        except IndexError:
+            last_update_fork= 'None'
+
+       
+        print(last_update_fork)
+        print('-----------')
+        
+
+        try:
+            commits_behind= chrome.find_elements_by_css_selector('div[class="d-flex flex-auto"]')[0].text
+        except NoSuchElementException:
+            commits_behind = 'None'
+        except IndexError:
+            commits_behind = 'None'
+            
+
+        print(commits_behind)
+        print('-----------')
+
+        time.sleep(3)
+
+    repositoryArray.append(
+        Repository(last_update_source, forks, url_fork,last_update_fork, commits_behind))
+    url_fork.clear()
+    
+    time.sleep(5)
        
     
 
